10efc4e4d7630e53e0f104cdbf9bd5f7af3d653a
VijayVekariya <vijayvekariya007@gmail.com>
2024-02-16 19:35:56 +0530

    16 Feb

---
+++ lowercase.js
@@ -0,0 +1,33 @@
+// Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello"
+// Output: "hello"
+// Example 2:
+
+// Input: s = "here"
+// Output: "here"
+// Example 3:
+
+// Input: s = "LOVELY"
+// Output: "lovely"
+ 
+
+
+
+
+
+let lowercaseword= function(s){
+
+    return s.toLowerCase();
+}
+
+let s = "Delhi Is Capital Of INDIA";
+
+let r = lowercaseword(s);
+console.log(r);
+
+// console.log(data.toLowerCase());
--- poweroftwo.js
+++
@@ -1,9 +0,0 @@
-var createCounter = function(n) {
-    var count = n;
-    return function() {
-        return count++;
-    };
-}
-
-let a = createCounter(2);
-console.log(a);
